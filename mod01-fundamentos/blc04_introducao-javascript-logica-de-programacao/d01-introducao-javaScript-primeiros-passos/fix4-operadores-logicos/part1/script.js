2f0c0caeeb85cf344f45801feaab05b7a13c2f56
brunoro7 <bruno.ro7c@gmail.com>
2021-12-01 14:57:55 -0300

    entregando exercicio part1, do dia01 em operadores.

---
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d01-introducao-javaScript-primeiros-passos/fix4-operadores-logicos/part1/script.js
@@ -0,0 +1,25 @@
+/** ======= Declarando ======= */
+
+const currentHour = 7;
+let message = "";
+
+/** ======= Determinando as regras ======= */
+
+if(currentHour >= 22){
+    message = "Não deveríamos comer nada, é hora de dormir"
+}
+else if(currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar :D"
+}
+else if(currentHour >= 14 && currentHour < 18){
+    message = "Vamos fazer um bolo pro café da tarde?"
+}
+else if(currentHour >= 11 && currentHour < 14){
+    message = "Hora do almoço!!!"
+}
+else if(currentHour < 11 && currentHour >= 4){
+    message = "Hmmm, cheiro de café recém passado"
+}
+
+/** ======= Imprimindo resultado ======= */
+console.log(message);
