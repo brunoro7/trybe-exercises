67c5aac4a68e53a03f508b6b3a94d869ff2f646d
brunoro7 <bruno.ro7c@gmail.com>
2021-12-06 01:30:03 -0300

    finalizando exercicio switch/case.

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d01-introducao-javaScript-primeiros-passos/fix5-switch-case/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d01-introducao-javaScript-primeiros-passos/fix5-switch-case/script.js
@@ -1,10 +1,18 @@
-let resultadoCandidato;
+let resultadoCandidato = "aprovada";
 
 switch(resultadoCandidato){
-    case "aprovada":
-
     
-    case "reprovada":
-
     case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break
+    case "reprovada":
+        console.log("Você está na lista de espera");
+        break
+    case "lista":
+         console.log("Você foi reprovada(o)");
+        break
+    default: 
+        console.log('não se aplica');
+        break
 }
+
