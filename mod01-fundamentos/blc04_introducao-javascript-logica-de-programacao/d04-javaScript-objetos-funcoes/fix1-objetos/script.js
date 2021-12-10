83fc853174a75e8d29975317f2da0be856fcae9d
brunoro7 <bruno.ro7c@gmail.com>
2021-12-09 23:30:04 -0300

    entregando requisito 1.

---
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d04-javaScript-objetos-funcoes/fix1-objetos/script.js
@@ -0,0 +1,9 @@
+/** ======= Declarando objeto player com as variáveis ======= */
+
+    let player = {
+        name: 'Marta',
+        lastName: 'Silva',
+        age: 34,
+        medals: { golden: 2, silver: 3 },    
+    };
+console.log(player);
