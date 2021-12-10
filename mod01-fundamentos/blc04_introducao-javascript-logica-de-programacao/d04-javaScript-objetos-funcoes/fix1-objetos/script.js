e66244df6301df9499675949524ec400a0831eb9
brunoro7 <bruno.ro7c@gmail.com>
2021-12-09 23:30:56 -0300

    entregando reuisito 2

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d04-javaScript-objetos-funcoes/fix1-objetos/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d04-javaScript-objetos-funcoes/fix1-objetos/script.js
@@ -7,3 +7,10 @@
         medals: { golden: 2, silver: 3 },    
     };
 console.log(player);
+
+/** ======= Acessando as chaves e concatenando na frase ======= */    
+    
+let fraseJogadora = "A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade";
+
+console.log(fraseJogadora);
+
