b6a4501a1233c8436e2155af64d557cdba28cbba
brunoro7 <bruno.ro7c@gmail.com>
2021-12-14 23:23:18 -0300

    entregando requisito1.

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d01-introducao-javaScript-primeiros-passos/naPratica/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d01-introducao-javaScript-primeiros-passos/naPratica/script.js
@@ -1,1 +1,21 @@
 /** ======= 5 Programas, um pra cada operação aritimética ======= */
+
+let valorA = 5;
+let valorB = 5;
+
+// Soma 
+let soma = valorA + valorB;
+// Subtração
+let subtracao = valorA - valorB;
+// Multiplicação
+let mutliplicacao = valorA * valorB;
+// Divisão
+let divisao = valorA / valorB;
+// Módulo
+let moduloResto = valorA % valorB;
+
+console.log(soma);
+console.log(subtracao);
+console.log(mutliplicacao);
+console.log(divisao);
+console.log(moduloResto);
