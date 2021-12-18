3e2c11485fbc3987f52f30d6ff69239abdc52abb
brunoro7 <bruno.ro7c@gmail.com>
2021-12-18 17:09:07 -0300

    entregando exercicio2.

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/fix1-arrays/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/fix1-arrays/script.js
@@ -4,3 +4,11 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+/** ======= exercicio 2 - obter o índice "Portfólio" ======= */
+//Obs: alterei o menu p/ menu1, para não dar "conflito";
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
