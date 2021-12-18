008ea183d116196827d5580c04ff4a6a5ed47344
brunoro7 <bruno.ro7c@gmail.com>
2021-12-18 17:16:19 -0300

    finalizando fix1.

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/fix1-arrays/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/fix1-arrays/script.js
@@ -3,7 +3,7 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
-console.log(menuServices);
+console.log("Resposta Do Exercício 1 - " + menuServices);
 
 /** ======= exercicio 2 - obter o índice "Portfólio" ======= */
 //Obs: alterei o menu p/ menu1, para não dar "conflito";
@@ -11,11 +11,11 @@ console.log(menuServices);
 let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(indexOfPortfolio);
+console.log("Resposta Do Exercício 2 - " + indexOfPortfolio);
 
 /** ======= exercicio 3 - adicionar o valor "Contato"======= */
 //Obs: alterei o menu p/ menu3, para não dar "conflito";
 
 let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu3.push("Contato");
-console.log(menu3);
+console.log("Resposta Do Exercício 3 - " + menu3);
