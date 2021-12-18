f95bba6848b507e1b8a4ffcf20eb2452f310031f
brunoro7 <bruno.ro7c@gmail.com>
2021-12-18 17:12:28 -0300

    entregando exercicio3.

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/fix1-arrays/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/fix1-arrays/script.js
@@ -12,3 +12,10 @@ let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
 console.log(indexOfPortfolio);
+
+/** ======= exercicio 3 - adicionar o valor "Contato"======= */
+//Obs: alterei o menu p/ menu3, para não dar "conflito";
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push("Contato");
+console.log(menu3);
