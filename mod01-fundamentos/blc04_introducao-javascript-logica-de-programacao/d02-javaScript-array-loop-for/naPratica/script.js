7f8a90975242b26f9dcb00f6a747f9ba6e3fbf8f
brunoro7 <bruno.ro7c@gmail.com>
2021-12-18 19:03:53 -0300

    entregando exercicio3.

--- mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/naPratica/script.js
+++ mod01-fundamentos/blc04_introducao-javascript-logica-de-programacao/d02-javaScript-array-loop-for/naPratica/script.js
@@ -1,27 +1,32 @@
 /** ======= Usar o mesmo array do 1 ao 7 ======= */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 /** ======= 1 - Imprimir todos valores ======= */
-
-console.log("Exercício - 1");
+  console.log("Exercício - 1");
 
   for (let cadaNumber of numbers){
   console.log(cadaNumber);
-}
+  }
 
 /** ======= 2 - Somar todos os valores do array ======= */
-
   console.log("Exercício - 2");
   
-let somaValoresArray = 0;
-for (let somaNumber=0; somaNumber < numbers.length; somaNumber+=1){
-  somaValoresArray = somaValoresArray + numbers[somaNumber];  
-}
-console.log(somaValoresArray);
+  let somaValoresArray = 0;
+  for (let somaNumber=0; somaNumber < numbers.length; somaNumber+=1){
+    somaValoresArray = somaValoresArray + numbers[somaNumber];  
+  }
+  
+  console.log(somaValoresArray);
 
+/** ======= 3 - Média aritimética do Array ======= */
+  console.log("Exercício - 3");
 
-/** ======= 3 ======= */
+  let arraySomado = somaValoresArray;
+  let divisorDoArray = numbers.length;
+  let resultMedArit = arraySomado / divisorDoArray;
+  
+  console.log(resultMedArit);
 
 /** ======= 4 ======= */
 
