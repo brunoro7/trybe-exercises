37248f7a031b00491c62c4d9407e4fd6d0f0e86f
brunoro7 <bruno.ro7c@gmail.com>
2021-12-24 13:34:33 -0300

    alterando background do header.

--- mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
+++ mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
@@ -1,22 +1,23 @@
 /** ======= 1- Criar as let, com os getEletments ou query ======= */
+let caixaTitulo = document.getElementById("header-container");
+  caixaTitulo.style.backgroundColor = "green";
 
-
-#container 
+// #container 
     
-  #header-container
+//   #header-container
   
-  .emergency-tasks 
+//   .emergency-tasks 
   
-  .emergency-tasks div 
-  .emergency-tasks h3 
+//   .emergency-tasks div 
+//   .emergency-tasks h3 
   
   
-  .no-emergency-tasks 
+//   .no-emergency-tasks 
   
-  .no-emergency-tasks div
-  .no-emergency-tasks h3 
+//   .no-emergency-tasks div
+//   .no-emergency-tasks h3 
   
-#footer-container
+// #footer-container
 
 
 
