6c7dc4b68beb91807a1da86067195778ca6cdb0e
brunoro7 <bruno.ro7c@gmail.com>
2021-12-24 14:06:27 -0300

    alterando background do #footer-container.

--- mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
+++ mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
@@ -1,19 +1,24 @@
-/** ======= Altera background do header ======= */
+/** ======= Altera background do header-container ======= */
+
 let caixaTitulo = document.getElementById("header-container");
   caixaTitulo.style.backgroundColor = "green";
 
 
 /** ======= Altera background do .emergency-tasks ======= */ 
-  let caixaUrgente = document.querySelectorAll(".emergency-tasks div");
+
+let caixaUrgente = document.querySelectorAll(".emergency-tasks div");
   for(let urgente of caixaUrgente){
       urgente.style.backgroundColor = "salmon";
   }
 
 /** ======= Altera background do .no-emergency-tasks ======= */
-  let caixaNaoUrgente = document.querySelectorAll(".no-emergency-tasks div");
+
+let caixaNaoUrgente = document.querySelectorAll(".no-emergency-tasks div");
   for(let naoUrgente of caixaNaoUrgente){
-      naoUrgente.style.backgroundColor = "yellow";
+      naoUrgente.style.backgroundColor = "gold";
   }
 
-  
-// #footer-container
+/** ======= Altera background do #footer-conteiner ======= */
+
+let caixaRodape = document.getElementById("footer-container");
+  caixaRodape.style.backgroundColor = "darkgreen";
