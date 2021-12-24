022194ea7c2267dea760a539a9bb415b8b3434f0
brunoro7 <bruno.ro7c@gmail.com>
2021-12-24 13:19:18 -0300

    começando a separação de ids e classes no js, para trabalhar.

---
+++ mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
@@ -0,0 +1,27 @@
+/** ======= 1- Criar as let, com os getEletments ou query ======= */
+
+
+#container 
+    
+  #header-container
+  
+  .emergency-tasks 
+  
+  .emergency-tasks div 
+  .emergency-tasks h3 
+  
+  
+  .no-emergency-tasks 
+  
+  .no-emergency-tasks div
+  .no-emergency-tasks h3 
+  
+#footer-container
+
+
+
+
+
+
+
+/** ======= 2- Acrescentar o mnínimo solicitado ======= */
