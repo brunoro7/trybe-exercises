282e3331cb36f002f973358f4b5744928da27ebd
brunoro7 <bruno.ro7c@gmail.com>
2021-12-25 22:18:59 -0300

    arrumando observações.

--- mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
+++ mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d01-javaScript-DOM-seletores/fix2/script.js
@@ -1,38 +1,38 @@
-/** ======= Altera background do header-container ======= */
+/** ======= Altera a cor do background do header-container ======= */
 
 let caixaTitulo = document.getElementById("header-container");
   caixaTitulo.style.backgroundColor = "green";
 
 
-/** ======= Altera background do .emergency-tasks ======= */ 
+/** ======= Altera a cor do background do .emergency-tasks ======= */ 
 
 let caixaUrgente = document.querySelectorAll(".emergency-tasks div");
   for(let urgente of caixaUrgente){
       urgente.style.backgroundColor = "salmon";
   }
 
-/** ======= Altera background do .emergency-tasks h3 ======= */ 
+/** ======= Altera a cor do background do .emergency-tasks h3 ======= */ 
 
 let fundoSubTituloUrg = document.querySelectorAll(".emergency-tasks h3");
   for(let subTituloUrg of fundoSubTituloUrg){
     subTituloUrg.style.backgroundColor = "darkviolet";
   }
 
-/** ======= Altera background do .no-emergency-tasks ======= */
+/** ======= Altera a cor do background do .no-emergency-tasks ======= */
 
 let caixaNaoUrgente = document.querySelectorAll(".no-emergency-tasks div");
   for(let naoUrgente of caixaNaoUrgente){
       naoUrgente.style.backgroundColor = "gold";
   }
 
-/** ======= Altera background do .no-emergency-tasks h3 ======= */ 
+/** ======= Altera a cor do background do .no-emergency-tasks h3 ======= */ 
 
 let fundoSubTituloNoUrg = document.querySelectorAll(".no-emergency-tasks h3");
   for(let subTituloNoUrg of fundoSubTituloNoUrg){
     subTituloNoUrg.style.backgroundColor = "black";
   }
 
-/** ======= Altera background do #footer-conteiner ======= */
+/** ======= Altera a cor do background do #footer-conteiner ======= */
 
 let caixaRodape = document.getElementById("footer-container");
   caixaRodape.style.backgroundColor = "darkgreen";
