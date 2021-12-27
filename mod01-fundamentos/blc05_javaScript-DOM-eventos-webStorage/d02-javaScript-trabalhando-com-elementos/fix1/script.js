5a748a40286605baf21587b0eccdf6267e9bf1e1
brunoro7 <bruno.ro7c@gmail.com>
2021-12-27 13:00:59 -0300

    entregando requisito1.

---
+++ mod01-fundamentos/blc05_javaScript-DOM-eventos-webStorage/d02-javaScript-trabalhando-com-elementos/fix1/script.js
@@ -0,0 +1,18 @@
+// #paiDoPai
+
+// #pai
+        
+// #primeiroFilho
+
+// #elementoOndeVoceEsta
+   
+// #primeiroFilhoDoFilho
+
+// #segundoEUltimoFilhoDoFilho
+
+// #terceiroFilho
+
+// #quartoEUltimoFilho
+
+let elementoOndeVcEsta = document.getElementById("elementoOndeVoceEsta");
+console.log(elementoOndeVcEsta);
