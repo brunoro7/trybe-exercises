f0a1f1fb21efeab655984e5b72d2de85e64f9471
brunoro7 <bruno.ro7c@gmail.com>
2022-01-04 15:20:51 -0300

    entregando requisito1.

--- mod01-fundamentos/blc06_html-css-forms-flexbox-responsivo/d01-html-css-forms/fix1-parte4-preventDefault/script.js
+++ mod01-fundamentos/blc06_html-css-forms-flexbox-responsivo/d01-html-css-forms/fix1-parte4-preventDefault/script.js
@@ -2,3 +2,11 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+
+/** ======= Função que "bloqueia" o padrão de abrir o link automáticamente ======= */
+
+  function blockHref (event) {
+    event.preventDefault();
+  }
+  HREF_LINK.addEventListener("click", blockHref);
