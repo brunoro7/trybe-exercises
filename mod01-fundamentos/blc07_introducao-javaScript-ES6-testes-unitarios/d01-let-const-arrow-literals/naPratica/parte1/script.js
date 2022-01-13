73342a231c33c4fe1ac9f7b4ea285ddb6b0dcaeb
brunoro7 <bruno.ro7c@gmail.com>
2022-01-13 16:26:00 -0300

    alteração no js.

--- mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d01-let-const-arrow-literals/naPratica/parte1/script.js
+++ mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d01-let-const-arrow-literals/naPratica/parte1/script.js
@@ -1,3 +1,5 @@
+/** ======= requisito 1 ======= */
+
 function testingScope(escopo) {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -9,5 +11,13 @@ function testingScope(escopo) {
   }
   // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
-
 testingScope(true);
+
+/** ======= requisito 2 ======= */
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+// será necessário alterar essa linha 😉
+console.log(oddsAndEvens);
