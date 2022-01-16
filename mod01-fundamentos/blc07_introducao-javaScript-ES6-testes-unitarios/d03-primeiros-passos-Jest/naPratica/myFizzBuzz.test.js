a097b58438344610fe2c5b57998291ba284b1a0f
brunoro7 <bruno.ro7c@gmail.com>
2022-01-16 02:03:29 -0300

    entregando requisito 4, do exercicio 3.

--- mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d03-primeiros-passos-Jest/naPratica/myFizzBuzz.test.js
+++ mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d03-primeiros-passos-Jest/naPratica/myFizzBuzz.test.js
@@ -2,7 +2,7 @@ const myFizzBuzz = require('./myFizzBuzz');
 
 describe('Requisito 1', () => {
   test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
-    expect(myFizzBuzz(15)).toMatch('fizz');
+    expect(myFizzBuzz(15)).toMatch('fizzbuzz');
     // neste caso, a linha 5 tb aceitaria .toString
   });
 });
@@ -17,5 +17,11 @@ describe('Requisito 2', () => {
 describe('Requisito 3', () => {
   test('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
     (myFizzBuzz(10)).toString('buzz');
+  });
+});
+
+describe('Requisito 4', () => {
+  test('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    (myFizzBuzz(7)).toString(false);
   });
 });
