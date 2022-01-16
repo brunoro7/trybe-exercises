5763f8f4e825819495a2235b82ef81e8fff52b99
brunoro7 <bruno.ro7c@gmail.com>
2022-01-15 21:55:27 -0300

    resolvendo requisito 3, do exercicio 2.

--- mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d03-primeiros-passos-Jest/naPratica/myRemove.test.js
+++ mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d03-primeiros-passos-Jest/naPratica/myRemove.test.js
@@ -11,3 +11,9 @@ describe('Requisito 2', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
 });
+
+describe('Requisito 3', () => {
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 5), retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
