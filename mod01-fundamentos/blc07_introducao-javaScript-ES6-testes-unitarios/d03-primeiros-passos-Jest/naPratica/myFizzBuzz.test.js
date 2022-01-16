074a87e55a4ed7e0b4fceaf33f4f39278fbd8ecc
brunoro7 <bruno.ro7c@gmail.com>
2022-01-15 22:26:17 -0300

    criando os arquivos myFizzBuzz, e já atendendo o requisito 1, do exercicio 3.

---
+++ mod01-fundamentos/blc07_introducao-javaScript-ES6-testes-unitarios/d03-primeiros-passos-Jest/naPratica/myFizzBuzz.test.js
@@ -0,0 +1,8 @@
+const myFizzBuzz = require('./myFizzBuzz');
+
+describe('Requisito 1', () => {
+  test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toMatch('fizz');
+    // neste caso, a linha 5 tb aceitaria .toString
+  });
+});
