fbf8f4b094988525100529615cd0c2187c7a55f7
brunoro7 <bruno.ro7c@gmail.com>
2022-06-30 05:49:03 -0300

    feat: continuação do exercicio.

--- mod03-back-end/bloco23/d02-Camada-Controller-Service/src/models/cepModel.js
+++ mod03-back-end/bloco23/d02-Camada-Controller-Service/src/models/cepModel.js
@@ -6,6 +6,12 @@ const cepModel = {
     const [cepList] = await db.query(sql);
     return cepList;
   },
+  async getByCep(cepValue) {
+    // console.log(cepValue);
+    const sql = 'SELECT * FROM cep_lookup.ceps WHERE cep = ?';
+    const [cepItem] = await db.query(sql, [cepValue]);
+    return cepItem;
+  },
 };
 
 module.exports = cepModel;
